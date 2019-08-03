e6f96bb6c8aa1550b2c6cf4514ee8fbf0117dec8
Mukhamat Jafar <jafar.mukhamat7@gmail.com>
2019-08-03 23:57:58 +0700

    tugas_18_js
    tugas sudah ok !!!

---
+++ tugas-18a.js
@@ -0,0 +1,6 @@
+function panggilslice(){
+
+  var kata = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout";
+  console.log(kata.slice(8,24));
+}
+panggilslice();
